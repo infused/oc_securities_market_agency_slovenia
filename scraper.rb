46fdf044469873ce69863eb285743aeafa718d7c
Keith Morrison <keithm@infused.org>
2015-01-01 14:33:12 -0800

    scrape banks

--- scraper.rb
+++ scraper.rb
@@ -3,7 +3,11 @@
 require 'json'
 require 'mechanize'
 require 'turbotlib'
-require 'byebug'
+require 'active_support'
+
+def clean_string(s)
+  s.gsub(/\A[[:space:]]*(.*?)[[:space:]]*\z/) { $1 }
+end
 
 urls = {
   'Investment Services' => 'http://www.atvp.si/Default.aspx?id=98'
@@ -14,18 +18,23 @@ urls.each do |category, url|
   page = agent.get(url)
   page.search('.documentContent .tabela tr:not(.header)').each do |bank|
 
+    company_name, *address_parts = bank.search('td')[1].text.strip.split("\r\n")
+    contact = bank.search('td')[2].text.strip.split("\r\n")
+    telephone_line = contact.detect {|x| x.match(/tel/)}
+    telephone = telephone_line && telephone_line.gsub(/(telefon|tel.):/, '').strip
+    fax_line = contact.detect {|x| x.match(/faks/)}
+    fax = fax_line && fax_line.gsub('faks:', '').strip
+
     data = {
-      # todo strip strange utf-8 blanks from beginning and end of string
-      company_name: bank.search('td')[1].text.strip.split("\r\n").first.strip,
-      # president: bank.search('td')[1].text.strip.split("\r\n").first,
-      # general_manager: bank.search('td')[2].text.strip.split("\r\n").first,
-      # address: bank.search('td')[3].text.strip.gsub(/\r\n/, ', ').squeeze(' '),
-      # telephone: bank.search('td')[4].text.strip.gsub(/\r\n/, ', ').squeeze(' '),
-      # fax: (bank.search('td')[5].text.strip.gsub(/\r\n/, ', ').squeeze(' ') rescue ''),
-      # url: (bank.search('td')[0].search('a')[0].attr('href') rescue ''),
-      # category: category,
-      # source_url: url,
-      # sample_date: Time.now
+      company_name: clean_string(company_name),
+      address: clean_string(address_parts.map {|x| x.strip}.join(', ')),
+      telephone: clean_string(telephone),
+      fax: clean_string(fax),
+      url: bank.search('td')[1].search('a').attr('href'),
+      member_of_stock_exchange: bank.search('td')[6].search('img').size > 0,
+      category: category,
+      source_url: url,
+      sample_date: Time.now
     }
 
     puts JSON.dump(data)
